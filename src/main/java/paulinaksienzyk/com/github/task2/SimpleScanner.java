bc0c19c0bb89b3b4b71a53c532f0eb7039074e0f
Paulina Ksienzyk <pilnicka8@gmail.com>
2021-03-11 16:25:32 +0100

    Add task2 - simple scanner

---
+++ src/main/java/paulinaksienzyk/com/github/task2/SimpleScanner.java
@@ -0,0 +1,14 @@
+package paulinaksienzyk.com.github.task2;
+
+/**
+ * Your task is to write a simple scanner that reads floating point numbers.
+ */
+public class SimpleScanner {
+
+    // TODO: Write a program that keeps asking the user for number so long as the user’s input is not a valid
+    //  floating-point number. If the user enters valid input, the loop quits.
+    public static void main(String[] args) {
+
+    }
+
+}
